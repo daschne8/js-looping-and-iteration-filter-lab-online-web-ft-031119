a12b4be43c516a3b267499ce65ed0a34b4015ee3
Daniel Schneider <daschne8@gmail.com>
2019-06-12 20:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function filter (collection, cb) {
-  const newCollection = [];
-
-  for (const user of collection) {
-    if (cb(user)) {
-      newCollection.push(user);
-    }
-  }
-
-  return newCollection;
-}
-
 function findMatching(drivers,name){
   return drivers.filter(function (driver){return driver.toLowerCase() === name.toLowerCase()})
 }
